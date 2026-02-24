4372862d2fdd87eb7954ddc467db60841de09778
pierrea5atwit <pierrea5@wit.edu>
2026-02-23 21:08:56 -0500

    Upd win_shell.h to match win_shell.c

--- windows_shell.h
+++ windows_shell.h
@@ -4,7 +4,7 @@
 #include <windows.h>
 
 // Helper function to build command line from arguments
-void build_command_line(char* args[], char* cmd_line, size_t max_len);
+void build_command_line(const char* args[], char* cmd_line, size_t max_len);
 
 // Main Windows shell function
 int run_windows_shell(void);
